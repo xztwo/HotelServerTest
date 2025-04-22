a8ebd3313adf2dda4b8a22bfc8ac8f9275e2ec27
Mikhail-BAV-Tvardovsky <aleksejbotev96@gmail.com>
2025-04-22 15:33:22 +0300

    Добавлен отдельный сервис авторизации, не проверен

---
+++ HotelServer.Auth/Data/AuthDbContext.cs
@@ -0,0 +1,34 @@
+﻿using HotelServer.Auth.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelServer.Auth.Data
+{
+    public class AuthDbContext : DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.Username)
+                    .IsRequired()
+                    .HasMaxLength(50);
+                entity.Property(u => u.PasswordHash)
+                    .IsRequired()
+                    .HasMaxLength(255);
+                entity.Property(u => u.Role)
+                    .IsRequired()
+                    .HasMaxLength(20);
+                
+                entity.HasIndex(u => u.Username)
+                    .IsUnique();
+            });
+        }
+    }
+} 
